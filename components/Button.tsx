56bc0b7238115fb15004c7f11ba6d3b1e8a83e55
Norberto Rubulotta <nrubulotta24@gmail.com>
2024-03-05 17:03:07 +0100

    GetApp and footer components finished.

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
 
 type ButtonProps = {
-    type: 'button' | 'submit';
-    title: string;
-    icon?: string;
-    variant: string;
-}
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: string;
+  full?: boolean;
+};
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
-    return (
-        <button type={type} className={`flexCenter gap-3 rounded-full ${variant}`}>{icon && <Image src={icon} alt={title} width={24} height={24} />}
-            <label className="bold-15 whitespace-nowrap">{title}</label>
-        </button>
-    )
-}
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 rounded-full border ${variant}  ${
+        full && "w-full"
+      }`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-15 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
 
-export default Button
+export default Button;
